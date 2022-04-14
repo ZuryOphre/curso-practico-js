60d1883ec15689bc14686eaa72e402fdb42115d2
ZuryOphere <zury.martinez.saldivia@gmail.com>
2022-04-14 12:45:50 -0400

    se creo constantes y modifico a funcion para calcular descuentos en el archivo .descuentos.js

---
+++ descuentos.js
@@ -0,0 +1,17 @@
+let precioOriginal = 120;
+let descuento = 18;
+
+
+function calcularPrecioConDescuentos(precio, descuento) {
+    let porcentajePrecioConDescuento = 100 - descuento;
+    let precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    
+    return precioConDescuento;
+}
+
+//console.log({
+//    precioOriginal,
+//    descuento,
+//    porcentajePrecioConDescuento,
+//    precioConDescuento,
+//});
